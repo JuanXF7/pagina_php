71e23ff2db8c8b0b303e812805f400b5d14e63c3
JuanXF7 <j7f7g7r7@gmail.com>
2026-04-14 10:14:48 -0500

    Resolviendo conflictos

--- pagina_php/conection.php
+++ pagina_php/conection.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "mysql";
-$user = "usuario_alimentos";
-$password = "123456";
-$db = "alimentos_db";
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$db = getenv('DB_NAME');
 
 $conexion = new mysqli($host, $user, $password, $db);
 
